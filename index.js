a6aa71c70a66ce48674b002261f243ab4f48e781
Monia-L <monia.laoufi@gmail.com>
2020-04-28 01:36:07 +0200

    Run migrations, seeds and build web app at build step

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
 const { Item } = require("./data-layer/models");
 
 const app = express();
+const PATH_TO_WEB_APP_BUILD = "web-app/build";
+app.use(express.static(path.join(__dirname, PATH_TO_WEB_APP_BUILD)));
 
-app.get("/api/items", async (req, res) => {
-  res.send({ items: await Item.findAll() });
-});
+app
+  .get("/api/items", async (req, res) => {
+    res.send({ items: await Item.findAll() });
+  })
+  .get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, PATH_TO_WEB_APP_BUILD, "index.html"));
+  });
 
 const PORT = process.env.PORT || 8007;
 
